0a090ffa4c096cef3907a107ea08e0283de184de
hsuchen314 <cheney0314@gmail.com>
2023-05-22 23:24:11 +0800

    first commit

---
+++ app/src/main/java/com/example/chatroom/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.chatroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button_account=findViewById(R.id.button2);
+        button_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
